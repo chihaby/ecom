0c0be210b021471efed62bd1cdcd065de6d921c1
Rad <chihaby@gmail.com>
2019-07-12 22:40:25 -0700

    frontend pages

--- sick-fits/frontend/.next/static/runtime/webpack.js
+++ sick-fits/frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "14837449f7049a1e7fd5";
+/******/ 	var hotCurrentHash = "134d9ce73dedb9e31498";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
